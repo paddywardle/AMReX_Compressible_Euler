17880b0a332f7a6959759cdde7a0bbc93fb5a2a6
Paddy Wardle <paddy.wardle@yahoo.com>
2023-02-17 11:16:16 +0000

    EulerEOS.cpp - updating from Eigen to var_array typedef

--- Source/EulerEOS.cpp
+++ Source/EulerEOS.cpp
@@ -3,43 +3,39 @@
 EulerEOS::EulerEOS(double gamma)
   :gamma(gamma){};
 
-Eigen::ArrayXXf EulerEOS::prim_to_con(Eigen::ArrayXXf u_p)
+var_array EulerEOS::prim_to_con(var_array u_p)
 {
-  Eigen::ArrayXXf u_c(u_p.rows(), u_p.cols());
+  var_array u_c;
 
-  u_c.col(0) = u_p.col(0);
-  u_c.col(1) = u_p.col(0) * u_p.col(1);
-  u_c.col(2) = u_p.col(2) / (gamma - 1.0) + 0.5 * u_p.col(0) * pow(u_p.col(1), 2);
+  u_c[0] = u_p[0];
+  u_c[1] = u_p[0] * u_p[1];
+  u_c[2] = u_p[2] / (gamma - 1.0) + 0.5 * u_p[0] * pow(u_p[1], 2);
 
   return u_c;
 }
 
-Eigen::ArrayXXf EulerEOS::con_to_prim(Eigen::ArrayXXf u_c)
+var_array EulerEOS::con_to_prim(var_array u_c)
 {
-  Eigen::ArrayXXf u_p(u_c.rows(), u_c.cols());
+  var_array u_p;
 
-  u_p.col(0) = u_c.col(0);
-  u_p.col(1) = u_c.col(1) / u_c.col(0);
-  u_p.col(2) = (u_c.col(2) - 0.5 * u_c.col(0) * pow(u_p.col(1), 2.0)) * (gamma - 1.0);
+  u_p[0] = u_c[0];
+  u_p[1] = u_c[1] / u_c[0];
+  u_p[2] = (u_c[2] - 0.5 * u_c[0] * pow(u_p[1], 2.0)) * (gamma - 1.0);
   
   return u_p;
 }
-Eigen::ArrayXXf EulerEOS::Euler_flux_fn(Eigen::ArrayXXf f, Eigen::ArrayXXf f_prim)
+var_array EulerEOS::Euler_flux_fn(var_array f, var_array f_prim)
 {
   
-  Eigen::ArrayXXf flux_fn(f.rows(), f.cols());
-  
-  for (int i=0; i<f.rows(); i++)
-    {
-      double rho = f(i, 0);
-      double energy = f(i, 2);
-      double velocity = f_prim(i, 1);
-      double pressure = f_prim(i, 2);
+  var_array flux_fn;
+  double rho = f(i, 0);
+  double energy = f(i, 2);
+  double velocity = f_prim(i, 1);
+  double pressure = f_prim(i, 2);
       
-      flux_fn(i, 0) = flux_fn_rho(rho, velocity);
-      flux_fn(i, 1) = flux_fn_mom(rho, velocity, pressure);
-      flux_fn(i, 2) = flux_fn_E(energy, velocity, pressure);
-    }
+  flux_fn[0] = flux_fn_rho(rho, velocity);
+  flux_fn[1] = flux_fn_mom(rho, velocity, pressure);
+  flux_fn[2] = flux_fn_E(energy, velocity, pressure);
 
   return flux_fn;
 
