c23e568c6702e5c6e9db9664417ebc55fcd9e625
Paddy Wardle <paddy.wardle@yahoo.com>
2023-03-06 12:45:55 +0000

    InitialCondTests - starting to add 2D tests

--- Source/InitialCondTests.cpp
+++ Source/InitialCondTests.cpp
@@ -1,9 +1,9 @@
 #include "InitialCondTests.h"
 
-var_array InitialCondTests::Test1D_1(double x)
+var_1D InitialCondTests::Test1D_1(double x)
 {
 
-  var_array initial_conds;
+  var_1D initial_conds;
   
   if (x < 0.5)
     {
@@ -27,10 +27,10 @@ var_array InitialCondTests::Test1D_1(double x)
   return initial_conds;
 }
 
-var_array InitialCondTests::Test1D_2(double x)
+var_1D InitialCondTests::Test1D_2(double x)
 {
 
-  var_array initial_conds;
+  var_1D initial_conds;
   
   if (x < 0.5)
     {
@@ -54,10 +54,10 @@ var_array InitialCondTests::Test1D_2(double x)
   return initial_conds;
 }
 
-var_array InitialCondTests::Test1D_3(double x)
+var_1D InitialCondTests::Test1D_3(double x)
 {
 
-  var_array initial_conds;
+  var_1D initial_conds;
   
   if (x < 0.5)
     {
@@ -81,10 +81,10 @@ var_array InitialCondTests::Test1D_3(double x)
   return initial_conds;
 }
 
-var_array InitialCondTests::Test1D_4(double x)
+var_1D InitialCondTests::Test1D_4(double x)
 {
 
-  var_array initial_conds;
+  var_1D initial_conds;
   
   if (x < 0.5)
     {
@@ -108,10 +108,10 @@ var_array InitialCondTests::Test1D_4(double x)
   return initial_conds;
 }
 
-var_array InitialCondTests::Test1D_5(double x)
+var_1D InitialCondTests::Test1D_5(double x)
 {
 
-  var_array initial_conds;
+  var_1D initial_conds;
   
   if (x < 0.5)
     {
@@ -135,4 +135,188 @@ var_array InitialCondTests::Test1D_5(double x)
   return initial_conds;
 }
 
+/*
+
+var_2D InitialCondTests::Test2D_1(double x, double y)
+{
+
+  var_2D initial_conds;
+  
+  if (x < 0.5)
+    {
+      // Density
+      initial_conds[0] = 1.0;
+      // Velocity
+      initial_conds[1] = 0.0;
+      // Pressure
+      initial_conds[2] = 1.0;
+
+      if (y < 0.5)
+	{
+	  initial_conds[3] = 0.0;
+	}
+      else
+	{
+	  initial_conds[3] = 0.0;
+	}
+      
+    }
+  else
+    {
+      // Density
+      initial_conds[0] = 0.125;
+      // Velocity
+      initial_conds[1] = 0.0;
+      // Pressure
+      initial_conds[2] = 0.1;
+
+      if (y < 0.5)
+	{
+	  initial_conds[3] = 0.0;
+	}
+      else
+	{
+	  initial_conds[3] = 0.0;
+	}
+    }
+
+  return initial_conds;
+}
+
+var_2D InitialCondTests::Test2D_2(double x, double y)
+{
+
+  var_2D initial_conds;
+  
+  if (x < 0.5)
+    {
+      // Density
+      initial_conds[0] = 1.0;
+      // Velocity
+      initial_conds[1] = -2.0;
+      // Pressure
+      initial_conds[2] = 0.4;
+
+      if (y < 0.5)
+	{
+	  initial_conds[3] = -2.0;
+	}
+      else
+	{
+	  initial_conds[3] = 0.0;
+	}
+      
+    }
+  else
+    {
+      // Density
+      initial_conds[0] = 1.0;
+      // Velocity
+      initial_conds[1] = 2.0;
+      // Pressure
+      initial_conds[2] = 0.4;
+    }
+
+  return initial_conds;
+}
+
+var_1D InitialCondTests::Test1D_3(double x)
+{
+
+  var_1D initial_conds;
+  
+  if (x < 0.5)
+    {
+      // Density
+      initial_conds[0] = 1.0;
+      // Velocity
+      initial_conds[1] = 0.0;
+      // Pressure
+      initial_conds[2] = 1000.0;
+    }
+  else
+    {
+      // Density
+      initial_conds[0] = 1.0;
+      // Velocity
+      initial_conds[1] = 0.0;
+      // Pressure
+      initial_conds[2] = 0.01;
+    }
+
+  return initial_conds;
+}
+
+var_1D InitialCondTests::Test1D_4(double x)
+{
+
+  var_1D initial_conds;
+  
+  if (x < 0.5)
+    {
+      // Density
+      initial_conds[0] = 1.0;
+      // Velocity
+      initial_conds[1] = 0.0;
+      // Pressure
+      initial_conds[2] = 0.01;
+    }
+  else
+    {
+      // Density
+      initial_conds[0] = 1.0;
+      // Velocity
+      initial_conds[1] = 0.0;
+      // Pressure
+      initial_conds[2] = 100.0;
+    }
+
+  return initial_conds;
+}
+
+var_2D InitialCondTests::Test2D_5(double x, double y)
+{
+
+  var_2D initial_conds;
+  
+  if (x < 0.5)
+    {
+      // Density
+      initial_conds[0] = 5.99924;
+      // Velocity
+      initial_conds[1] = 19.5975;
+      // Pressure
+      initial_conds[2] = 460.894;
+
+      if (y < 0.5)
+	{
+	  initial_conds[3] = 19.5975;
+	}
+      else
+	{
+	  initial_conds[3] = -6.19633;
+	}
+    }
+  else
+    {
+      // Density
+      initial_conds[0] = 5.99242;
+      // Velocity
+      initial_conds[1] = -6.19633;
+      // Pressure
+      initial_conds[2] = 46.0950;
+
+      if (y < 0.5)
+	{
+	  initial_conds[3] = 19.5975;
+	}
+      else
+	{
+	  initial_conds[3] = -6.19633;
+	}
+    }
+
+  return initial_conds;
+}
+*/
 
--- Source/InitialCondTests.h
+++ Source/InitialCondTests.h
@@ -1,6 +1,8 @@
 #include <array>
 
-typedef std::array<double,3> var_array;
+typedef std::array<double,3> var_1D;
+typedef std::array<double,4> var_2D;
+
 
 class InitialCondTests
 {
@@ -8,13 +10,27 @@ class InitialCondTests
 
   InitialCondTests(){};
   
-  var_array Test1D_1(double);
+  var_1D Test1D_1(double);
 
-  var_array Test1D_2(double);
+  var_1D Test1D_2(double);
 
-  var_array Test1D_3(double);
+  var_1D Test1D_3(double);
 
-  var_array Test1D_4(double);
+  var_1D Test1D_4(double);
 
-  var_array Test1D_5(double);
+  var_1D Test1D_5(double);
+
+  /*
+
+  var_2D Test2D_1(double);
+
+  var_2D Test2D_2(double);
+
+  var_2D Test2D_3(double);
+
+  var_2D Test2D_4(double);
+
+  var_2D Test2D_5(double);
+  */
+
 };
