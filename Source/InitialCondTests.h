7cec2bfce7fc4b83c00ad5557253b24d2fa817fd
Paddy Wardle <paddy.wardle@yahoo.com>
2023-03-09 12:57:30 +0000

    InitialCondTests update adding y and diagonal tests

--- Source/InitialCondTests.h
+++ Source/InitialCondTests.h
@@ -1,8 +1,6 @@
 #include <array>
 
-typedef std::array<double,4> var_1D;
-typedef std::array<double,4> var_2D;
-
+typedef std::array<double,4> var_array;
 
 class InitialCondTests
 {
@@ -10,27 +8,46 @@ class InitialCondTests
 
   InitialCondTests(){};
   
-  var_1D Test1D_1(double);
+  var_array Test1D_1(double);
 
-  var_1D Test1D_2(double);
+  var_array Test1D_2(double);
 
-  var_1D Test1D_3(double);
+  var_array Test1D_3(double);
 
-  var_1D Test1D_4(double);
+  var_array Test1D_4(double);
 
-  var_1D Test1D_5(double);
+  var_array Test1D_5(double);
 
+  var_array Test1D_1Y(double);
+
+  var_array Test1D_2Y(double);
+
+  var_array Test1D_3Y(double);
+
+  var_array Test1D_4Y(double);
+
+  var_array Test1D_5Y(double);
+
+  var_array Test1_diag(double, double);
+
+  var_array Test2_diag(double, double);
+
+  var_array Test3_diag(double, double);
+
+  var_array Test4_diag(double, double);
+
+  var_array Test5_diag(double, double);
+  
   /*
+  var_array Test2D_1(double);
 
-  var_2D Test2D_1(double);
+  var_array Test2D_2(double);
 
-  var_2D Test2D_2(double);
+  var_array Test2D_3(double);
 
-  var_2D Test2D_3(double);
+  var_array Test2D_4(double);
 
-  var_2D Test2D_4(double);
-
-  var_2D Test2D_5(double);
+  var_array Test2D_5(double);
   */
 
 };
